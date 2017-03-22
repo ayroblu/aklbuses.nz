289c55a3cbb032e344b3ec9683a024f5b384f20c
Ben Lu <ben@ben-l.com>
2017-03-23 00:19:43 +1300

    Added many methods, seem redundant, mobile is different than desktop...

--- src/Marker.js
+++ src/Marker.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 
 import './Marker.css'
 
+function cn(){
+  return Array.from(arguments).filter(a=>a).join(' ')
+}
 export default class Marker extends Component{
   static propTypes = {
     route: React.PropTypes.object.isRequired
@@ -10,7 +13,7 @@ export default class Marker extends Component{
   render(){
     return (
       <div className='Marker' style={Object.assign({}, (this.props.$hover || this.props.active) ? {background: '#5bf', zIndex: 1} : {})}>
-        <div className={'Popup' + (this.props.active ? ' active' : '')}>
+        <div className={cn('Popup',this.props.active ? ' active' : '', this.props.$focus ? 'focused' : '')} style={Object.assign({}, (this.props.$hover || this.props.active) ? {opacity: 1} : {})}>
           <p>{this.props.route.agency_id + ' - ' + this.props.route.route_short_name}</p>
           <p>{this.props.route.route_long_name}</p>
         </div>
