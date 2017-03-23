f7ea9d862119e60b510d0a1f4fe75294bb2f70b3
Ben Lu <ben@ben-l.com>
2017-03-24 09:11:06 +1300

    Fix route naming hotfix

--- src/Marker.js
+++ src/Marker.js
@@ -7,10 +7,18 @@ function cn(){
 }
 export default class Marker extends Component{
   static propTypes = {
-    route: React.PropTypes.object.isRequired
+    route: React.PropTypes.object
   , active: React.PropTypes.bool
   }
   render(){
+    let { route } = this.props
+    if (!route) {
+      route = {
+        agency_id: 'Unknown Agency'
+      , route_short_name: 'Route Name'
+      , route_long_name: 'Route Long Name'
+      }
+    }
     return (
       <div className='Marker' style={Object.assign({}, (this.props.$hover || this.props.active) ? {background: '#5bf', zIndex: 1} : {})}>
         <div className={cn('Popup',this.props.active ? ' active' : '', this.props.$focus ? 'focused' : '')} style={Object.assign({}, (this.props.$hover || this.props.active) ? {opacity: 1} : {})}>
