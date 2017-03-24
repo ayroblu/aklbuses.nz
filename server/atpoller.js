865f8634015dd92040f9eeb4b99d05c82f03b2b3
Ben Lu <ben@ben-l.com>
2017-03-24 20:18:22 +1300

    Use a merge structure to merge in changes

--- server/atpoller.js
+++ server/atpoller.js
@@ -4,7 +4,7 @@ let store = {}
 
 function run(){
   setInterval(getLocations, 30000) // every 30 seconds
-  setInterval(getRoutes, 86400000) // daily: 1000 * 60 * 60 * 24
+  //setInterval(getRoutes, 86400000) // daily: 1000 * 60 * 60 * 24
   getRoutes()
   getLocations()
   console.log('Run at poller')
@@ -13,6 +13,10 @@ function getLocations(){
   const atApi = new AtApi()
   return atApi.getVehicleLocations().then(locations=>{
     store.locations = locations.response
+    if (!store.routes || locations.response.entity.find(e=>!store.routes[e.vehicle.trip.route_id])){
+      console.log('no routes')
+      getRoutes()
+    }
     console.log('New locations:', locations.response.entity.length)
   }).catch(err=>{
     console.error('Error getting routes', err)
@@ -21,10 +25,14 @@ function getLocations(){
 function getRoutes(){
   const atApi = new AtApi()
   return atApi.getRoutes().then(routes=>{
-    store.routes = routes.response.reduce((a,n)=>{
+    const newRoutes = routes.response.reduce((a,n)=>{
       a[n.route_id] = n
       return a
     }, {})
+    if (!store.routes){
+      store.routes = {}
+    }
+    Object.assign(store.routes, newRoutes)
     console.log('New routes:', routes.response.length)
   }).catch(err=>{
     console.error('Error getting routes', err)
