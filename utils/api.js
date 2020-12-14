916edba0e5bbb3c66d70363ab676f7c63a6dae02
Ben Lu <ayroblu@gmail.com>
2020-12-14 14:40:47 +0000

    Prettier more

--- utils/api.js
+++ utils/api.js
@@ -1,73 +1,85 @@
-const fetch = require('node-fetch')
+const fetch = require("node-fetch");
 
-function getVehicleLocations(){
-  var url = 'https://api.at.govt.nz/v1/public/realtime/vehiclelocations?api_key=34893bb6-1d38-4403-923b-f3892de693e4'
-  getUrl(url, saveVehicleLocations)
+function getVehicleLocations() {
+  var url =
+    "https://api.at.govt.nz/v1/public/realtime/vehiclelocations?api_key=34893bb6-1d38-4403-923b-f3892de693e4";
+  getUrl(url, saveVehicleLocations);
 }
-function getRoutes(){
-  var url = 'https://api.at.govt.nz/v1/gtfs/routes?api_key=34893bb6-1d38-4403-923b-f3892de693e4'
-  getUrl(url, updateRoutes)
+function getRoutes() {
+  var url =
+    "https://api.at.govt.nz/v1/gtfs/routes?api_key=34893bb6-1d38-4403-923b-f3892de693e4";
+  getUrl(url, updateRoutes);
 }
-const apiUrl = 'https://api.at.govt.nz'
-const api_key = '34893bb6-1d38-4403-923b-f3892de693e4'
+const apiUrl = "https://api.at.govt.nz";
+const api_key = "34893bb6-1d38-4403-923b-f3892de693e4";
 
 class Api {
-  constructor(options){
-    this.apiUrl = apiUrl
-    this.api_key = api_key
-    this.prefix = ''
-    if (!options){
-      return
+  constructor(options) {
+    this.apiUrl = apiUrl;
+    this.api_key = api_key;
+    this.prefix = "";
+    if (!options) {
+      return;
     }
-    const {token} = options
-    this.token = token
+    const { token } = options;
+    this.token = token;
   }
-  _getJsonHeaders(){
+  _getJsonHeaders() {
     return {
-      'Accept': 'application/json'
-    }
+      Accept: "application/json",
+    };
   }
-  _postJsonHeaders(){
+  _postJsonHeaders() {
     return {
-      'Accept': 'application/json'
-    , 'Content-Type': 'application/json'
-    }
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    };
   }
-  setToken(token){
-    this.token = token
+  setToken(token) {
+    this.token = token;
   }
-  _buildQueryString(data){
-    return '?' + Object.keys(data).map(d=>d+'='+encodeURIComponent(data[d]))
+  _buildQueryString(data) {
+    return (
+      "?" + Object.keys(data).map((d) => d + "=" + encodeURIComponent(data[d]))
+    );
   }
-  _fetchData(url){
+  _fetchData(url) {
     return fetch(url, {
-      headers: this._getJsonHeaders()
-    }).then(resp=>{
-      if (resp.ok){
-        return resp.json()
+      headers: this._getJsonHeaders(),
+    }).then((resp) => {
+      if (resp.ok) {
+        return resp.json();
       }
-      console.log('Status:', resp.status)
-      resp.text().then(text=>console.error('Api error', text))
-      return new Promise(()=>{})
-    })
+      console.log("Status:", resp.status);
+      resp.text().then((text) => console.error("Api error", text));
+      return new Promise(() => {});
+    });
   }
 }
 
 class AtApi extends Api {
-  constructor(options){
-    super(options)
-    this.prefix = '/v1'
+  constructor(options) {
+    super(options);
+    this.prefix = "/v1";
   }
-  getVehicleLocations(){
-    const url = this.apiUrl + this.prefix + '/public/realtime/vehicleLocations' + this._buildQueryString({api_key})
-    return this._fetchData(url)
+  getVehicleLocations() {
+    const url =
+      this.apiUrl +
+      this.prefix +
+      "/public/realtime/vehicleLocations" +
+      this._buildQueryString({ api_key });
+    return this._fetchData(url);
   }
-  getRoutes(){
-    const url = this.apiUrl + this.prefix + '/gtfs/routes' + this._buildQueryString({api_key})
-    return this._fetchData(url)
+  getRoutes() {
+    const url =
+      this.apiUrl +
+      this.prefix +
+      "/gtfs/routes" +
+      this._buildQueryString({ api_key });
+    return this._fetchData(url);
   }
 }
 
 module.exports = {
-  AtApi
-}
+  AtApi,
+};
--- utils/downloader.js
+++ utils/downloader.js
@@ -1,66 +1,70 @@
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
-var ObjectId = require('mongodb').ObjectID;
-var fetch = require('node-fetch')
+var MongoClient = require("mongodb").MongoClient;
+var assert = require("assert");
+var ObjectId = require("mongodb").ObjectID;
+var fetch = require("node-fetch");
 
-var mongoUrl = 'mongodb://localhost:27017/atdata';
+var mongoUrl = "mongodb://localhost:27017/atdata";
 
 // node script to download
 //var apiKey = '34893bb6-1d38-4403-923b-f3892de693e4'
 
-function getUrl(url, callback){
-  fetch(url).then(res=>{
-    if (res.ok)
-      return res.json()
-    console.error(res)
-    throw new Error('Not okay response')
-  }).then(json=>{
-    callback(json.response)
-  }).catch(err=>{
-    console.error(err)
-  })
+function getUrl(url, callback) {
+  fetch(url)
+    .then((res) => {
+      if (res.ok) return res.json();
+      console.error(res);
+      throw new Error("Not okay response");
+    })
+    .then((json) => {
+      callback(json.response);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 }
-function getVehicleLocations(){
-  var url = 'https://api.at.govt.nz/v1/public/realtime/vehiclelocations?api_key=34893bb6-1d38-4403-923b-f3892de693e4'
-  getUrl(url, saveVehicleLocations)
+function getVehicleLocations() {
+  var url =
+    "https://api.at.govt.nz/v1/public/realtime/vehiclelocations?api_key=34893bb6-1d38-4403-923b-f3892de693e4";
+  getUrl(url, saveVehicleLocations);
 }
-function getRoutes(){
-  var url = 'https://api.at.govt.nz/v1/gtfs/routes?api_key=34893bb6-1d38-4403-923b-f3892de693e4'
-  getUrl(url, updateRoutes)
+function getRoutes() {
+  var url =
+    "https://api.at.govt.nz/v1/gtfs/routes?api_key=34893bb6-1d38-4403-923b-f3892de693e4";
+  getUrl(url, updateRoutes);
 }
-var saveVehicleLocations = function(data){
-  var insertVehicleLocations = function(db, data, callback) {
-    db.collection('vehicle_location').insertOne(data, function(err, result) {
+var saveVehicleLocations = function (data) {
+  var insertVehicleLocations = function (db, data, callback) {
+    db.collection("vehicle_location").insertOne(data, function (err, result) {
       assert.equal(err, null);
       console.log("Inserted a document into the vehicle_location collection.");
       callback();
     });
   };
-  MongoClient.connect(mongoUrl, function(err, db) {
+  MongoClient.connect(mongoUrl, function (err, db) {
     assert.equal(null, err);
-    insertVehicleLocations(db, data, function() {
+    insertVehicleLocations(db, data, function () {
       db.close();
     });
   });
-}
-var updateRoutes = function(data){
-  var runUpdate = function(db, data, callback) {
-    db.collection('routes').drop()
-    db.collection('routes').insert(data, function(err, result) {
+};
+var updateRoutes = function (data) {
+  var runUpdate = function (db, data, callback) {
+    db.collection("routes").drop();
+    db.collection("routes").insert(data, function (err, result) {
       assert.equal(err, null);
       console.log("Inserted documents into the routes collection.");
       callback();
     });
   };
-  MongoClient.connect(mongoUrl, function(err, db) {
+  MongoClient.connect(mongoUrl, function (err, db) {
     assert.equal(null, err);
-    runUpdate(db, data, function() {
+    runUpdate(db, data, function () {
       db.close();
     });
   });
-}
+};
 
 module.exports = {
-  getVehicleLocations
-, getRoutes
-}
+  getVehicleLocations,
+  getRoutes,
+};
